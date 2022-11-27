793218d49f3509aed139a8376e4f7381e9a4a7bf
Adesh12345 <adesh.kochar123@gmail.com>
2022-11-27 11:57:06 +0800

    Commit for New Flask Project

---
+++ tut2.py
@@ -0,0 +1,21 @@
+import flask
+from flask import Flask,render_template
+
+app = Flask(__name__)
+
+#Go till the end ...
+@app.route("/")
+def hello_world():
+    return "Hello, World!"
+
+@app.route("/about")
+def aboutTheApp():
+    name="Agora"
+    return render_template('about.html', name2 = name)
+
+@app.route("/app")
+def runTheApp():
+    return render_template('bootstrap.html')
+
+#Automatially run on the server
+app.run(debug=True)
